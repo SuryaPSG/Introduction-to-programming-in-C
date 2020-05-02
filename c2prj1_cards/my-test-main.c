79e8eb6c2ef7ce4dcec49c235e108e7aa0981ac5
Coursera Learner <nobody@nowhere.nul>
2020-05-02 09:29:31 +0000

    Did ny assignment poker

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,16 @@
 #include "cards.h"
 
 int main(void) {
-
+  card_t t1;
+  t1.value=2;
+  t1.suit=SPADES;
+  assert_card_valid(t1);
+  char v='J';
+  char s='s';
+  card_t t2=card_from_letters(v,s);
+  card_t t3=card_from_num(51);
+  print_card(t1);
+  print_card(t2);
+  print_card(t3);
+  return 0;
 }
