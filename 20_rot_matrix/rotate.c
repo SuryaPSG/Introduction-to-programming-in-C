2e72672d7257cdec4db0d36dd5ba5d6c4185e2a7
Coursera Learner <nobody@nowhere.nul>
2020-05-07 10:50:47 +0000

    Did my assignment 20

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void rotate(char matrix[10][10]){
+  //char *** p=&matrix;
+  char dup[10][10];
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      dup[i][j]=matrix[i][j];
+    }
+  }
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      matrix[i][j]=dup[9-j][i];
+    }
+  }
+}
