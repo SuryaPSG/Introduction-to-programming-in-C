a892c79580340b3cfaad2c9e1a5f2ddc1d04edd3
Coursera Learner <nobody@nowhere.nul>
2020-05-16 07:03:59 +0000

    Did my assignment

---
+++ 27_matrix_input/rotateMatrix.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+void rotate(char matrix[10][10]){
+  char dup[10][10];
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      dup[i][j]=matrix[i][j];
+    }
+  }
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      matrix[i][j]=dup[9-j][i];
+    }
+  }
+}
+int main(int argc,char ** argv){
+  if(argc!=2){
+    fprintf(stderr,"Usage ./file input\n");
+    return EXIT_FAILURE;
+  }
+  FILE *f=fopen(argv[1],"r");
+  char str[12];
+  char matrix[10][10];
+  int i=0;
+  while(fgets(str,12,f)!=NULL){
+    if(strchr(str,'\n')==NULL){
+    fprintf(stderr,"FALIURE1\n");
+      return EXIT_FAILURE;
+    }
+    for(int j=0;j<10;j++){
+      if(str[i]=='\n'){
+	fprintf(stderr,"FALIURE2\n");
+	return EXIT_FAILURE;
+      }
+      matrix[i][j]=str[j];
+    }
+    i++;
+  }
+  if(i<10){
+    fprintf(stderr,"FALIURE3\n");
+    return EXIT_FAILURE;
+  }
+  if(i>10){
+    fprintf(stderr,"FALIURE4\n");
+    return EXIT_FAILURE;
+  }
+  rotate(matrix);
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++){
+      printf("%c",matrix[i][j]);
+    }
+    printf("\n");
+  }
+  if(fclose(f)!=0)
+    return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}
